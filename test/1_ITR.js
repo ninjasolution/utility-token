43c220f47873ed69ceef98103fe2f5a914f9c2cd
Ninja Solution <top1successfulman@gmail.com>
2023-09-05 08:27:33 +1000

    update

--- test/1_ITR.js
+++ test/1_ITR.js
@@ -9,23 +9,32 @@ function eth(amount) {
 
 describe("DREToken", function () {
 
+  let token, governace;
+  let deployer, target, fund;
+
   // We define a fixture to reuse the same setup in every test.
   // We use loadFixture to run this setup once, snapshot that state,
   // and reset Hardhat Network to that snapshot in every test.
   async function deploy() {
-    const [deployer, fund, target] = await ethers.getSigners();
+    const [_deployer, _fund, _target] = await ethers.getSigners();
 
     // deploy the uniswap v2 protocol
     const { factory, router, weth9 } = await UniswapV2Deployer.deploy(deployer);
 
     // deploy our token
     const Token = await ethers.getContractFactory("ITR")
-    const token = await Token.deploy()
+    token = await Token.deploy()
     await token.deployed()
 
+    // deploy our token
+    const ITRGovernor = await ethers.getContractFactory("ITRGovernor");
+    governace = await ITRGovernor.deploy();
+    await governace.deployed();
+
+    
     // approve the spending
-    await weth9.approve(router.address, eth(1000))
-    await token.approve(router.address, eth(1000))
+    await weth9.approve(router.address, eth(1000));
+    await token.approve(router.address, eth(1000));
 
     // add liquidity
     await router.addLiquidityETH(
@@ -33,18 +42,24 @@ describe("DREToken", function () {
       eth(500),
       eth(500),
       eth(10),
-      deployer.address,
+      _deployer.address,
       constants.MaxUint256,
       { value: eth(10) }
     )
 
-    return { token, deployer, fund, target, factory, router, weth9 }
+    deployer = _deployer;
+    target = _target;
+    fund = _fund;
   }
+
+  before(async () => {
+    await deploy();
+  })
+
 
   describe("transfer", function () {
 
     it("shouldn't tax on transfer", async function () {
-      const { token, deployer, fund, target } = await loadFixture(deploy)
 
       await expect(token.transfer(target.address, eth(100))).to.changeTokenBalances(
         token,
@@ -54,6 +69,21 @@ describe("DREToken", function () {
 
     })
 
+  })
+
+  describe("Governor", function() {
+    it("Create a proposal", async function() {
+
+      const grantAmount = eth(1000);
+      const transferCalldata = token.interface.encodeFunctionData("transfer", [target.address, grantAmount]);
+  
+      await governor.propose(
+        [token.address],
+        [0],
+        [transferCalldata],
+        "Proposal #1: Give grant to team"
+      );
+    })
   })
 
 });
